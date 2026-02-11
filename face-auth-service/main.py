d71facba0c06f3f4807cbc7527ab44cbf863293e
Unwuchola, Eneojo <eneojounwuchola@my.unt.edu>
2026-02-11 16:36:02 -0600

    Initialize database setup and integrate FastAPI

---
+++ face-auth-service/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from dbConnect import connect_to_postgres,test_postgres_connection
+app = FastAPI()
+
+
+    
+
+
+
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    test_postgres_connection()
